7981134e2f9df9b8d44e22c572f40e689a922c01
chotusinghrajput18 <chotusinghrajput57@gmail.com>
2026-08-26 23:35:00 +0530

    Initial Commit

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, redirect, url_for, render_template
+d=Flask(__name__)
+
+@d.route("/hello")
+def hello():
+    return "Hello World\nHello World\nHello World\nHello World\nHello World\nHello World" 
+
+@d.route("/python/<name>")
+def fname(name):
+    if name:
+        return f"Your name is {name}"
+    return 
+
+@d.route("/my/<mob>")
+def mobile(mob):
+    if mob:
+        return f"Your phone No: {mob}"
+    return "Nothing to show"
+
+@d.route("/myproject/<na>")
+def display(na):
+    if na=="chotu":
+        return redirect(url_for('fname',name=na))
+    else:
+        return redirect(url_for('mobile',mob=na))
+        
+
+@d.route("/myfunc")
+def fun():
+    return render_template("index.html")
+
+if __name__=="__main__":
+    d.run(debug=True)
---
+++ new.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, url_for, redirect
+my=Flask(__name__)
+
+@my.route("/")
+def ask():
+    return render_template("ask.html")
+
+@my.route("/put/<str:name>")
+def main(name):
+    return f"Your name is {name}"
+
+@my.route("/new/<str:new>")
+def display(new):
+    return redirect(url_for("main",name=new))
+
+if __name__=="__main__":
+    my.run(debug=True)
