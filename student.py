a973dfcc4d75e86f1b7991c69e64709755c298c8
chotusinghrajput18 <chotusinghrajput57@gmail.com>
2026-09-05 23:21:20 +0530

    new class

---
+++ student.py
@@ -0,0 +1,23 @@
+class Student:
+    def __init__(self, name, age, grade):
+        self.name = name
+        self.age = age
+        self.grade = grade
+
+    def get_name(self):
+        return self.name
+
+    def get_age(self):
+        return self.age
+
+    def get_grade(self):
+        return self.grade
+
+    def set_name(self, name):
+        self.name = name
+
+    def set_age(self, age):
+        self.age = age
+
+    def set_grade(self, grade):
+        self.grade = grade
