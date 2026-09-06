f72ba663af8c243fb4356ac2f1537cb036099e3b
chotusinghrajput18 <chotusinghrajput57@gmail.com>
2026-09-06 23:31:42 +0530

    calling functions

--- simplecalc.py
+++ simplecalc.py
@@ -1,14 +1,19 @@
 class Calculator:
-    def add(self, a, b):
-        return a + b
+    def add(self, a, *b):
+        return a +(sum(b))
 
     def subtract(self, a, b):
         return a - b
 
-    def multiply(self, a, b):
-        return a * b
+    def multiply(self, a, *b):
+        result = a
+        for x in b:
+            result *= x
+        return result
 
     def divide(self, a, b):
         if b == 0:
             raise ValueError("Cannot divide by zero.")
         return a / b
+c1=Calculator()
+print(c1.add(2,3,4,6,0,7,))
